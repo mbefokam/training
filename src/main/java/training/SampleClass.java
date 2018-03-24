18d34868488062f854b7570e6634e49310622ac0
Pierre Antoine <mbefokam@gmail.com>
2018-03-24 19:21:14 -0400

    first push for student to copy

---
+++ src/main/java/training/SampleClass.java
@@ -0,0 +1,10 @@
+package training;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Camer !!!");
+
+	}
+
+}
---
+++ src/test/java/training/TestSampleClass.java
@@ -0,0 +1,14 @@
+package training;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestSampleClass {
+	
+	@Test
+	public void test() {
+		 assertTrue(true);
+	}
+
+}
